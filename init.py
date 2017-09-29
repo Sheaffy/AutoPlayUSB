6857849af1964a37478c6997f588c2536f87a15a
Sheaffy <sheaffy.ashley@gmail.com>
2017-09-29 12:45:34 +0100

    Update comments

--- init.py
+++ init.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 import os, random, time
+from os import listdir
+from os.path import isfile, join
+
+
+#variable decliration
+LOCATION = ""
+
+
 
 partitionsFile = open("/proc/partitions")
 lines = partitionsFile.readlines()[2:]#Skips the header lines
-
-LOCATION = ""
-
 for line in lines:
     words = [x.strip() for x in line.split()]
     minorNumber = int(words[1])
@@ -19,31 +24,31 @@ for line in lines:
                 
 
 if LOCATION != "":
+
+	#Mounting found usb stick into local directory
 	os.system("sudo umount "+LOCATION)
 	print("Unmounted")
 	os.system("sudo mount "+LOCATION+" /home/AutoPlayUSB/mount") 
 	print("Mounted")
 
-	#GET MP3 FILE
-	from os import listdir
-	from os.path import isfile, join
+	# find all files in usb stick
 	mypath = "/home/AutoPlayUSB/mount/"
 	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 	print(onlyfiles)
 
-	#REMOVE FILE IF PRESENT
+	# remove playlist if present
 	os.system("rm /home/AutoPlayUSB/mount/playlist.txt")
-	#MAKE NEW FILE
+	# make new playlist file from found files
 	file = open('/home/AutoPlayUSB/mount/playlist.txt', 'w')
 	file.writelines(["%s\n" % item  for item in onlyfiles])
 	file.close()
 	
+	#Play auto files using mplayer with the created playlist.txt
 	time.sleep(2)
-
 	os.system("cd /home/AutoPlayUSB/mount && mplayer -ao alsa:device=hw=1.0 -playlist playlist.txt -loop 0")
 
 
-
+#create loop to keep the script running forever
 while True:
 	try:
 		pass
